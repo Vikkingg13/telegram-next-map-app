cf28589c7d777cf49fe6d16e2dff893ccce29f24
Vikkingg13 <vikkingg13@gmail.com>
2025-02-28 22:05:29 +0300

    Add navigation panel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import { Section, Cell, Image, List, Navigation, Button } from '@telegram-apps/telegram-ui';
+'use client'
+
+import { Section, Cell, Image, List, Navigation, Button, InlineButtons, Avatar } from '@telegram-apps/telegram-ui';
 import { useTranslations } from 'next-intl';
 
 import { Page } from '@/components/Page';
-import Map from '@/components/Map/Map';
+import SentimentSatisfiedAlt from '@mui/icons-material/SettingsAccessibilityOutlined'
 
 import dynamic from 'next/dynamic';
+import { InlineButtonsItem } from '@telegram-apps/telegram-ui/dist/components/Blocks/InlineButtons/components/InlineButtonsItem/InlineButtonsItem';
+import { Settings, TravelExplore } from '@mui/icons-material';
+import TravelExploreIcon from '@mui/icons-material/TravelExplore';
+import { initData, useSignal } from '@telegram-apps/sdk-react';
 
 const MapComponent = dynamic(
   () => import('@/components/Map/Map'), // импортируем как промис
@@ -14,12 +20,25 @@ const MapComponent = dynamic(
 
 export default function Home() {
   const t = useTranslations('i18n');
+  const initDataState = useSignal(initData.state);
 
   return (
-    <>
     <Page back={false}>
-    <MapComponent/>
+      <MapComponent/>
+      <InlineButtons mode="plain">
+        <InlineButtonsItem text="Настройки">
+          <Settings />
+        </InlineButtonsItem>
+        <InlineButtonsItem text="Осматриваться">
+        <TravelExplore/>
+        </InlineButtonsItem>
+        <InlineButtonsItem>
+        <Avatar
+          size={40}
+          src={initDataState?.user?.photoUrl}
+        />
+        </InlineButtonsItem>
+    </InlineButtons>
     </Page>
-    </>
   );
 }
